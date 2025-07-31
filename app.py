7de140b82fc4f43e0102b6b58102607b0f8b5c7f
Tim Buffington <timbuffington@gmail.com>
2025-07-30 17:54:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -338,7 +338,6 @@ if st.session_state.show_contact_form:
 # ──────────────────────────────
 # 🚀 Main UI
 # ──────────────────────────────
-apply_custom_css()
 
 st.markdown("<h1>EBOSS&reg;Model Selection Tool</h1>", unsafe_allow_html=True)
 
