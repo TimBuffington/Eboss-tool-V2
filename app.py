25e1d243dfee14ebf1960194b4f9fcc714f05326
Tim Buffington <timbuffington@gmail.com>
2025-07-31 20:39:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -79,9 +79,9 @@ input, select, textarea, .stSelectbox > div > div, .stTextInput > div > div {
 }
 input:focus, select:focus, textarea:focus, 
 .stSelectbox > div > div:focus, .stTextInput > div > div:focus {
-    background-color: #e0e0e0 !important
+    background-color: #111 !important
     border: 2px solid #81BD47 !important;
-    color: #111 !important;
+    color: #e0e0e0  !important;
     font-weight: bold !important;
     font-family: 'Montserrat', 'Segoe UI', Arial, sans-serif !important;
     text-shadow: none !important;
