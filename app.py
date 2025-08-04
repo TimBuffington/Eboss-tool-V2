bcb603b93e24b192aab6fb8cf8d40ad04aa9f0ad
Tim Buffington <timbuffington@gmail.com>
2025-08-04 04:21:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -365,10 +365,10 @@ def landing_page():
                 st.session_state.show_contact_form = True
                 st.session_state.landing_shown = False
                 st.rerun()
-
-       col3, col4 = st.columns(2)
-with col3:
-    st.markdown("""
+                
+                col3, col4 = st.columns(2)
+                with col3:
+                    st.markdown("""
     <a href="https://youtu.be/0Om2qO-zZfM?si=iTiPgIL2t-xDFixc" target="_blank" style="text-decoration: none; color: inherit;">
         <div style="
             min-width: 320px !important;
