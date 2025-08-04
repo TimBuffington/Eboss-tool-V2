d9cab804ec4e2147c707e440edaf5b38f8812bb8
Tim Buffington <timbuffington@gmail.com>
2025-08-03 18:43:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -575,8 +575,8 @@ def render_user_input_form():
         border: 2px solid #D3D3D3;
         border-radius: 12px;
         padding: 1rem;
-        min-height: 75px !important;
-        max-height: 100px !important;
+        min-height: 50px !important;
+        max-height: 75px !important;
         justify-content: center;
         align-items: center;
         z-index: 9999;
@@ -588,8 +588,8 @@ def render_user_input_form():
         color: #81BD47;
         text-align: center;
         padding: 0.25rem 0.25rem !important;
-        min-height: 75px !important;
-        max-height: 100px !important;
+        min-height: 50px !important;
+        max-height: 75px !important;
         justify-content: center;
         align-items: center;
         z-index: 9999;
@@ -602,8 +602,8 @@ def render_user_input_form():
         border: 1px solid #D3D3D3 !important;
         border-radius: 8px !important;
         padding: 0.25rem 0.25rem !important;
-        min-height: 60px !important;
-        max-height: 750px !important;
+        min-height: 50px !important;
+        max-height: 75px !important;
         justify-content: center;
         align-items: center;
         z-index: 9999;
@@ -615,9 +615,9 @@ def render_user_input_form():
         font-weight: bold;
         border: 1px solid #D3D3D3 !important;
         border-radius: 8px !important;
-        padding: 0.4rem !important;
-        min-height: 75px !important;
-        max-height: 100px !important;
+        padding: 0.25rem !important;
+        min-height: 50px !important;
+        max-height: 75px !important;
         justify-content: center;
         align-items: center;
         z-index: 9999;
