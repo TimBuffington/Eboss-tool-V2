69f3d45a00ce12f594073dd037dfb546c6bad0b4
Tim Buffington <timbuffington@gmail.com>
2025-08-05 00:24:51 -0700

    Add files via upload

---
+++ spec_data.py
@@ -0,0 +1,668 @@
+spec_data = {
+  "EBOSS 25 kVA": {
+    "Maximum Intermittent  Output": [
+      [
+        "Three-phase",
+        "30 kva / 24 kw"
+      ],
+      [
+        "Single-phase",
+        "20 kva / 16 kw"
+      ],
+      [
+        "Frequency",
+        "60 Hz"
+      ],
+      [
+        "Simultaneous voltage",
+        "120/240 (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Voltage regulation",
+        "Adjustable"
+      ],
+      [
+        "Max. Intermittent amp-load 208v",
+        "70 A / 13.5 kW"
+      ],
+      [
+        "Max. Intermittent amp-load 480v",
+        "30 A / 19 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "104 A / 19.5 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "45 A / 29 kW"
+      ]
+    ],
+    "Maximum Continuous  Output": [
+      [
+        "Generator",
+        "Airman SDG25"
+      ],
+      [
+        "Generator charge rate",
+        "25 kva / 20 kw"
+      ],
+      [
+        "Three-phase output",
+        "23 kva / 19 kw"
+      ],
+      [
+        "Single-phase output",
+        "20 kva / 16 kw"
+      ],
+      [
+        "Simultaneous voltage",
+        "120/240 (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Max. Continuous amp-load @208v",
+        "70 A / 13.5 kW"
+      ],
+      [
+        "Max. Continuous amp-load @480v",
+        "30 A / 19 kW"
+      ]
+    ],
+    "Battery capacity": [
+      [
+        "Total battery life or energy throughout",
+        "1,200 mwh"
+      ],
+      [
+        "Charge time (no load)",
+        "< 45 min"
+      ],
+      [
+        "Inverter output max",
+        "24 kw"
+      ],
+      [
+        "Parallel capability",
+        "Available"
+      ]
+    ],
+    "Battery type": [
+      [
+        "Est. Cycle life @ 77\u00b0f enclosure temp",
+        "90K Cycles at 90% DOD"
+      ],
+      [
+        "Est. Cycle life @ 100\u00b0f enclosure temp",
+        "80K Cycles at 90% DOD"
+      ]
+    ],
+    "Operating temperatures": [
+      [
+        "Inverter cold start temperature (minimum)",
+        "14\u00b0F"
+      ],
+      [
+        "Running operating temperature",
+        "-22\u00b0F to 130\u00b0F"
+      ],
+      [
+        "Arctic package operating temp. (optional)",
+        "-50\u00b0F to 130\u00b0F"
+      ]
+    ],
+    "Weights & Dimensions": [
+      [
+        "L x w x h x (Eboss only)",
+        "40\u201d x 48\u201d x 46\u201d"
+      ],
+      [
+        "Eboss weight only",
+        "2,120 lbs"
+      ],
+      [
+        "L x w x h (trailer and generator)",
+        "160\u201d x 74\u201d x 75\u201d"
+      ],
+      [
+        "Total weight (without/with fuel)",
+        "5100 / 5500 lbs"
+      ],
+      [
+        "Integral fuel tank capacity",
+        "51.5 gal"
+      ]
+    ]
+  },
+  "EBOSS 70 kVA": {
+    "Maximum Intermittent  Output": [
+      [
+        "Three-phase",
+        "70 kVA / 56 kW"
+      ],
+      [
+        "Single-phase",
+        "47 kVA / 37 kW"
+      ],
+      [
+        "Frequency",
+        "60 Hz"
+      ],
+      [
+        "Simultaneous voltage",
+        "120/240 (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Voltage regulation",
+        "Adjustable"
+      ],
+      [
+        "Max. Intermittent amp-load 208v",
+        "194 A 37.2 kW"
+      ],
+      [
+        "Max. Intermittent amp-load 480v",
+        "84 A / 55 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "291 A / 55 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "126 A / 175  kW"
+      ]
+    ],
+    "Maximum Continuous  Output": [
+      [
+        "Generator",
+        "Airman SDG45"
+      ],
+      [
+        "Generator charge rate",
+        "45 kVA / 36 kW"
+      ],
+      [
+        "Three-phase output",
+        "42 kVA / 33 kW"
+      ],
+      [
+        "Single-phase output",
+        "28 kVA / 22 kW"
+      ],
+      [
+        "Simultaneous voltage",
+        "120/240 (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Max. Continuous amp-load @208v",
+        "119 A / 22.8 kW"
+      ],
+      [
+        "Max. Continuous amp-load @480v",
+        "54 A 35 kW"
+      ]
+    ],
+    "Battery capacity": [
+      [
+        "Total battery life or energy throughout",
+        "2,000 MWh"
+      ],
+      [
+        "Charge time (no load)",
+        "\u2248 45 Minutes"
+      ],
+      [
+        "Inverter output max",
+        "56 kW"
+      ],
+      [
+        "Parallel capability",
+        "Available"
+      ]
+    ],
+    "Battery type": [
+      [
+        "Est. Cycle life @ 77\u00b0f enclosure temp",
+        "90K Cycles at 90% DOD"
+      ],
+      [
+        "Est. Cycle life @ 100\u00b0f enclosure temp",
+        "80K Cycles at 90% DOD"
+      ]
+    ],
+    "Operating temperatures": [
+      [
+        "Inverter cold start temperature (minimum)",
+        "14\u00b0F"
+      ],
+      [
+        "Running operating temperature",
+        "-22\u00b0F to 130\u00b0F"
+      ],
+      [
+        "Arctic package operating temp. (optional)",
+        "-50\u00b0F to 130\u00b0F"
+      ]
+    ],
+    "Weights & Dimensions": [
+      [
+        "L x w x h x (Eboss only)",
+        "55\u201d x 44\u201d x 62\u201d"
+      ],
+      [
+        "Eboss weight only",
+        "2,700 lbs"
+      ],
+      [
+        "L x w x h (trailer and generator)",
+        "167\u201d x 71\u201d x 76\u201d"
+      ],
+      [
+        "Total weight (without/with fuel)",
+        "6950 / 7600 lbs"
+      ],
+      [
+        "Integral fuel tank capacity",
+        "80.5 gal"
+      ]
+    ]
+  },
+  "EBOSS 125 kVA": {
+    "Maximum Intermittent  Output": [
+      [
+        "Three-phase",
+        "125 kVA / 100 kW"
+      ],
+      [
+        "Single-phase",
+        "N/A / N/A"
+      ],
+      [
+        "Frequency",
+        "60 Hz"
+      ],
+      [
+        "Simultaneous voltage",
+        "120V (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Voltage regulation",
+        "Adjustable"
+      ],
+      [
+        "Max. Intermittent amp-load 208v",
+        "345 A 66.2 kW"
+      ],
+      [
+        "Max. Intermittent amp-load 480v",
+        "150 A / 99 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "532 A / 291 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "231  / 532 kW"
+      ]
+    ],
+    "Maximum Continuous  Output": [
+      [
+        "Generator",
+        "Airman SDG65"
+      ],
+      [
+        "Generator charge rate",
+        "65 kVA / 52 kW"
+      ],
+      [
+        "Three-phase output",
+        "62 kVA / 50 kW"
+      ],
+      [
+        "Single-phase output",
+        "2.4 kW x 2"
+      ],
+      [
+        "Simultaneous voltage",
+        "120V (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Max. Continuous amp-load @208v",
+        "167 A / 32 kW"
+      ],
+      [
+        "Max. Continuous amp-load @480v",
+        "76 A 50 kW"
+      ]
+    ],
+    "Battery capacity": [
+      [
+        "Total battery life or energy throughout",
+        "4,000 MWh"
+      ],
+      [
+        "Charge time (no load)",
+        "= 1 Hour"
+      ],
+      [
+        "Inverter output max",
+        "100 kW"
+      ],
+      [
+        "Parallel capability",
+        "Available"
+      ]
+    ],
+    "Battery type": [
+      [
+        "Est. Cycle life @ 77\u00b0f enclosure temp",
+        "90K Cycles at 90% DOD"
+      ],
+      [
+        "Est. Cycle life @ 100\u00b0f enclosure temp",
+        "80K Cycles at 90% DOD"
+      ]
+    ],
+    "Operating temperatures": [
+      [
+        "Inverter cold start temperature (minimum)",
+        "14\u00b0F"
+      ],
+      [
+        "Running operating temperature",
+        "-22\u00b0F to 130\u00b0F"
+      ],
+      [
+        "Arctic package operating temp. (optional)",
+        "-50\u00b0F to 130\u00b0F"
+      ]
+    ],
+    "Weights & Dimensions": [
+      [
+        "L x w x h x (Eboss only)",
+        "\u2248 73.5\u201d x 61\u201d x 104.5\u201d"
+      ],
+      [
+        "Eboss weight only",
+        "9,600 lbs"
+      ],
+      [
+        "L x w x h (trailer and generator)",
+        "233\u201d x 52\u201d x 97\u201d"
+      ],
+      [
+        "Total weight (without/with fuel)",
+        "\u2248 18,000 lbs"
+      ],
+      [
+        "Integral fuel tank capacity",
+        "111 gal"
+      ]
+    ]
+  },
+  "EBOSS 220 kVA": {
+    "Maximum Intermittent  Output": [
+      [
+        "Three-phase",
+        "220 kVA / 176 kW"
+      ],
+      [
+        "Single-phase",
+        "N/A / N/A"
+      ],
+      [
+        "Frequency",
+        "60 Hz"
+      ],
+      [
+        "Simultaneous voltage",
+        "120V (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Voltage regulation",
+        "Adjustable"
+      ],
+      [
+        "Max. Intermittent amp-load 208v",
+        "700 A / 134 kW"
+      ],
+      [
+        "Max. Intermittent amp-load 480v",
+        "303 A / 201 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "1065 A / 204 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "461 A / 708 kW"
+      ]
+    ],
+    "Maximum Continuous  Output": [
+      [
+        "Generator",
+        "Airman SDG125"
+      ],
+      [
+        "Generator charge rate",
+        "125 kVA / 100 kW"
+      ],
+      [
+        "Three-phase output",
+        "120 kVA / 96 kW"
+      ],
+      [
+        "Single-phase output",
+        "2.4 kW x 2"
+      ],
+      [
+        "Simultaneous voltage",
+        "120V (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Max. Continuous amp-load @208v",
+        "328 A / 63 kW"
+      ],
+      [
+        "Max. Continuous amp-load @480v",
+        "150 A 99 kW"
+      ]
+    ],
+    "Battery capacity": [
+      [
+        "Total battery life or energy throughout",
+        "6,000 MWh"
+      ],
+      [
+        "Charge time (no load)",
+        "\u2248 50 Minutes"
+      ],
+      [
+        "Inverter output max",
+        "176 kW"
+      ],
+      [
+        "Parallel capability",
+        "Available"
+      ]
+    ],
+    "Battery type": [
+      [
+        "Est. Cycle life @ 77\u00b0f enclosure temp",
+        "90K Cycles at 90% DOD"
+      ],
+      [
+        "Est. Cycle life @ 100\u00b0f enclosure temp",
+        "80K Cycles at 90% DOD"
+      ]
+    ],
+    "Operating temperatures": [
+      [
+        "Inverter cold start temperature (minimum)",
+        "14\u00b0F"
+      ],
+      [
+        "Running operating temperature",
+        "-22\u00b0F to 130\u00b0F"
+      ],
+      [
+        "Arctic package operating temp. (optional)",
+        "-50\u00b0F to 130\u00b0F"
+      ]
+    ],
+    "Weights & Dimensions": [
+      [
+        "L x w x h x (Eboss only)",
+        "\u224873.5\u201d x 61\u201d x 104.5\u201d"
+      ],
+      [
+        "Eboss weight only",
+        "11,200 lbs"
+      ],
+      [
+        "L x w x h (trailer and generator)",
+        "\u2248249\u201d x 52\u201d x 97\u201d"
+      ],
+      [
+        "Total weight (without/with fuel)",
+        "\u224821,000 lbs"
+      ],
+      [
+        "Integral fuel tank capacity",
+        "168 gal"
+      ]
+    ]
+  },
+  "EBOSS 400 kVA": {
+    "Maximum Intermittent  Output": [
+      [
+        "Three-phase",
+        "400 kVA / 320 kW"
+      ],
+      [
+        "Frequency",
+        "60 Hz"
+      ],
+      [
+        "Simultaneous voltage",
+        "120V (Courtesy Outlets) \u2022 480 (3\u03a6)"
+      ],
+      [
+        "Voltage regulation",
+        "Adjustable"
+      ],
+      [
+        "Max. Intermittent amp-load 208v",
+        "481 A / 92.5 kW"
+      ],
+      [
+        "Max. Intermittent amp-load 480v",
+        "769 A / 319 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "1776 A / 341 kW"
+      ],
+      [
+        "Motor start rating for 3 second (208v)",
+        "769 A / 511 kW"
+      ]
+    ],
+    "Maximum Continuous  Output": [
+      [
+        "Generator",
+        "Airman SDG220"
+      ],
+      [
+        "Generator charge rate",
+        "220 kVA / 176 kW"
+      ],
+      [
+        "Three-phase output",
+        "210 kVA / 168 kW"
+      ],
+      [
+        "Single-phase output",
+        "2.4 kW x 2"
+      ],
+      [
+        "Simultaneous voltage",
+        "120V (1\u03a6) \u2022 208/480 (3\u03a6)"
+      ],
+      [
+        "Max. Continuous amp-load @208v",
+        "< 125 kW"
+      ],
+      [
+        "Max. Continuous amp-load @480v",
+        "264 A / 175 kW"
+      ]
+    ],
+    "Battery capacity": [
+      [
+        "Total battery life or energy throughout",
+        "10,000 MWh"
+      ],
+      [
+        "Charge time (no load)",
+        "\u2248 45 Minutes"
+      ],
+      [
+        "Inverter output max",
+        "320 kW"
+      ],
+      [
+        "Parallel capability",
+        "Available"
+      ]
+    ],
+    "Battery type": [
+      [
+        "Est. Cycle life @ 77\u00b0f enclosure temp",
+        "90K Cycles at 90% DOD"
+      ],
+      [
+        "Est. Cycle life @ 100\u00b0f enclosure temp",
+        "80K Cycles at 90% DOD"
+      ]
+    ],
+    "Operating temperatures": [
+      [
+        "Inverter cold start temperature (minimum)",
+        "14\u00b0F"
+      ],
+      [
+        "Running operating temperature",
+        "-22\u00b0F to 130\u00b0F"
+      ],
+      [
+        "Arctic package operating temp. (optional)",
+        "-50\u00b0F to 130\u00b0F"
+      ]
+    ],
+    "Weights & Dimensions": [
+      [
+        "L x w x h x (Eboss only)",
+        "\u2248 TBD"
+      ],
+      [
+        "Eboss weight only",
+        "11,037 lbs"
+      ],
+      [
+        "L x w x h (trailer and generator)",
+        "\u2248 262\u201d x 74\u201d x 75\u201d"
+      ],
+      [
+        "Total weight (without/with fuel)",
+        "\u2248 21,600 lbs / 24,000 lbs"
+      ],
+      [
+        "Integral fuel tank capacity",
+        "285 gal"
+      ]
+    ]
+  }
+}
