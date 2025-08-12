5ba0485453249ac9c0dcc36e325baf8f814767c6
Tim Buffington <timbuffington@gmail.com>
2025-08-12 01:04:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -252,7 +252,7 @@ st.markdown("<div class='main-center-stack'>", unsafe_allow_html=True)
 
 # Title
 
-st.markdown(<center><H1>"EBOSS® Size & Spec Tool"</H1></center>, unsafe_allow_html=False)
+st.markdown("<center><h1>EBOSS® Size & Spec Tool</h1></center>", unsafe_allow_html=True)
 
 #t.markdown("<center>Please Select a Configuration</center>", unsafe_allow_html=True)
 
