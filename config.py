c1b26e54ae04686fab853d79f59ec8de3ce6e19b
Tim Buffington <timbuffington@gmail.com>
2025-08-17 16:36:57 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,13 @@
 # config.py
 
-EBOSS_LOAD_REFERENCE = {
-    "battery_capacities": {
-        "EB25 kVA": 15,
-        "EB70 kVA": 25,
-        "EB125 kVA": 50,
-        "EB220 kVA": 75,
-        "EB400 kVA": 125
-    },
-    "generator_kva_hybrid": {
-        "EB25 kVA": 25,
-        "EB70 kVA": 45,
-        "EB125 kVA": 65,
-        "EB220 kVA": 125,
-        "EB400 kVA": 220
-    },
-    "generator_sizes": {
-        25: {"eboss_model": "EB25 kVA", "pm_charge_rate": 18.5, "fh_charge_rate": 19.5, "max_charge_rate": 20, "kwh": 15, "gen_kw": 20},
-        45: {"eboss_model": "EB70 kVA", "pm_charge_rate": 33, "fh_charge_rate": 36, "max_charge_rate": 45, "kwh": 25, "gen_kw": 36},
-        65: {"eboss_model": "EB125 kVA", "pm_charge_rate": 48, "fh_charge_rate": 52, "max_charge_rate": 65, "kwh": 50, "gen_kw": 52},
-        125: {"eboss_model": "EB220 kVA", "pm_charge_rate": 96, "fh_charge_rate": 100, "max_charge_rate": 125, "kwh": 75, "gen_kw": 100},
-        220: {"eboss_model": "EB400 kVA", "pm_charge_rate": 166, "fh_charge_rate": 176, "max_charge_rate": 220, "kwh": 125, "gen_kw": 176}
+SPECS = {
+    25:  {"eboss_model": "EB25 kVA",  "pm_charge_rate": 18.5, "fh_charge_rate": 19.5, "max_charge_rate": 20,  "kwh": 15,  "gen_kw": 20,  "max_cont_kw": 18,  "max_peak_kw": 20},
+    45:  {"eboss_model": "EB70 kVA",  "pm_charge_rate": 33.0, "fh_charge_rate": 36.0, "max_charge_rate": 45,  "kwh": 25,  "gen_kw": 36,  "max_cont_kw": 40,  "max_peak_kw": 56},
+    65:  {"eboss_model": "EB125 kVA", "pm_charge_rate": 48.0, "fh_charge_rate": 52.0, "max_charge_rate": 65,  "kwh": 50,  "gen_kw": 52,  "max_cont_kw": 58,  "max_peak_kw": 125},
+    125: {"eboss_model": "EB220 kVA", "pm_charge_rate": 96.0, "fh_charge_rate": 100.0,"max_charge_rate": 125, "kwh": 75,  "gen_kw": 100, "max_cont_kw": 112, "max_peak_kw": 252},
+    220: {"eboss_model": "EB400 kVA", "pm_charge_rate": 166.0,"fh_charge_rate": 176.0,"max_charge_rate": 220, "kwh": 125, "gen_kw": 176, "max_cont_kw": 198, "max_peak_kw": 639},
+}
+
     },
     "gph_interpolation": {
         25: {"25%": 0.67, "50%": 0.94, "75%": 1.26, "100%": 1.62},
@@ -88,3 +75,4 @@ STANDARD_GENERATOR_DATA = {
         "weight_lbs": 28000
     }
 }
+
