74df9a043b13f12fb8f3ccd0ab00cd8e0ce5acff
Tim Buffington <timbuffington@gmail.com>
2025-08-17 17:11:29 -0400

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -1,1 +1,8 @@
+SPECS = {
+    25:  {"eboss_model": "EB25 kVA",  "pm_charge_rate": 18.5, "fh_charge_rate": 19.5, "max_charge_rate": 20,  "kwh": 15,  "gen_kw": 20,  "max_cont_kw": 18,  "max_peak_kw": 20},
+    45:  {"eboss_model": "EB70 kVA",  "pm_charge_rate": 33.0, "fh_charge_rate": 36.0, "max_charge_rate": 45,  "kwh": 25,  "gen_kw": 36,  "max_cont_kw": 40,  "max_peak_kw": 56},
+    65:  {"eboss_model": "EB125 kVA", "pm_charge_rate": 48.0, "fh_charge_rate": 52.0, "max_charge_rate": 65,  "kwh": 50,  "gen_kw": 52,  "max_cont_kw": 58,  "max_peak_kw": 125},
+    125: {"eboss_model": "EB220 kVA", "pm_charge_rate": 96.0, "fh_charge_rate": 100.0,"max_charge_rate": 125, "kwh": 75,  "gen_kw": 100, "max_cont_kw": 112, "max_peak_kw": 252},
+    220: {"eboss_model": "EB400 kVA", "pm_charge_rate": 166.0,"fh_charge_rate": 176.0,"max_charge_rate": 220, "kwh": 125, "gen_kw": 176, "max_cont_kw": 198, "max_peak_kw": 639},
+}
 
