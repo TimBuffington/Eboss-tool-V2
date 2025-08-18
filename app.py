7febd4758a336c5ef279d29e856f7d3389f6aead
Tim Buffington <timbuffington@gmail.com>
2025-08-18 06:37:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import components.nav as nav
 from components.modals import open_config_modal
 
 st.set_page_config(page_title="EBOSS® Tool", layout="wide", initial_sidebar_state="collapsed")
-
+nav.render_global_header(mode="external")
 with st.container():
             cols = st.columns(3)
             with cols[1]:  # Use the middle column to help with centering
@@ -13,7 +13,7 @@ with st.container():
                 )
                         
                     
-nav.render_global_header(mode="external")
+
 choice = nav.render_config_selector()   # ← no troubleshooting arg anymore
 
 if choice in ("manual", "load_based", "fuel_eff"):
