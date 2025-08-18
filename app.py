05b897d05424e21bdcd0130ae1461df001d84583
Tim Buffington <timbuffington@gmail.com>
2025-08-18 05:50:08 -0400

    1

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from components.modals import open_config_modal
 
 st.set_page_config(page_title="EBOSS® Tool", layout="wide", initial_sidebar_state="collapsed")
 
-with st.container():
+with st.container(:
             cols = st.columns(3)
             with cols[1]:  # Use the middle column to help with centering
                 st.markdown(
                     "<div class='message-container'>Please Select EBOSS Configuration</div>",
-                    unsafe_allow_html=True,
+                ),unsafe_allow_html=True,
                     
 nav.render_global_header(mode="external")
 choice = nav.render_config_selector()   # ← no troubleshooting arg anymore
