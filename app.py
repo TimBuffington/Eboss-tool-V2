4eaad82701b252f27cd01075c18897535d3e9fb2
Tim Buffington <timbuffington@gmail.com>
2025-08-18 06:40:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,7 @@ from components.modals import open_config_modal
 
 st.set_page_config(page_title="EBOSS® Tool", layout="wide", initial_sidebar_state="collapsed")
 nav.render_global_header(mode="external")
-with st.container():
-            col = st.columns(1,1,1)
-            with cols[1]:  # Use the middle column to help with centering
-                st.markdown(
-                    "<div class='message-container'>Please Select EBOSS Configuration</div>",
-                     unsafe_allow_html=True,
-                )
+
                         
                     
 
