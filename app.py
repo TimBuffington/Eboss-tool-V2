8ec9e53c25a5423a9171ed02f5deee6c11457742
Tim Buffington <timbuffington@gmail.com>
2025-08-17 23:06:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ from components.modals import open_config_modal
 st.set_page_config(page_title="EBOSS® Tool", layout="wide", initial_sidebar_state="collapsed")
 
 render_global_header(mode="external")
-st.markdown("<div class='message-text'>Please Select an EBOSS Configuration</div>", unsafe_allow_html=True)
-
 choice = render_config_selector(include_troubleshooting=True)
+
 if choice in ("manual", "load_based", "fuel_eff"):
     open_config_modal(choice)
 elif choice == "troubleshooting":
