7878b5e24b286e875d597411fdad29e76578b717
Tim Buffington <timbuffington@gmail.com>
2025-12-04 11:29:54 -0800

    Update styles.py

--- styles.py
+++ styles.py
@@ -1,7 +1,7 @@
 def global_css():
     
-st.markdown("""
-<style>
+    st.markdown("""
+    <style>
     /* Import fonts */
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
     
