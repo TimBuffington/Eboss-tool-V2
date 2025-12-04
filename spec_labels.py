4d63d83998800dead4a75bc51f06fe4a2efa4979
Tim Buffington <timbuffington@gmail.com>
2025-12-03 18:34:39 -0800

    Update spec_labels.py

--- spec_labels.py
+++ spec_labels.py
@@ -6,3 +6,17 @@ SPEC_LABELS = [
     "Dimensions",
     "Warranty"
 ]
+
+#return {
+        "model_capacity": model_capacity,
+        "peak_utilization": peak_utilization,
+        "continuous_utilization": continuous_utilization,
+        "charge_rate": charge_rate,
+        "battery_capacity": battery_capacity,
+        "charge_time": charge_time,
+        "fuel_consumption_gph": fuel_consumption,
+        "fuel_per_day": fuel_consumption * 24 if fuel_consumption else 0,
+        "co2_per_day": co2_per_day,
+        "engine_load_percent": engine_load_percent,
+        "generator_data": generator_data
+    }
